75835ca6746995097ced42c4e92d0ad769e3e47a
Alfatha Fitrah Insan <elfathfitrah@gmail.com>
2018-09-13 01:31:55 +0700

    Add files via upload

---
+++ js/myscript.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	
+	$(".button-collapse").sideNav();
+
+	$('.slider').slider({
+		full_width: true
+	});
+	
+	$(".dropdown-button").dropdown({ hover: false });
+	
+});
